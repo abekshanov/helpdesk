68829f23e86b531fb2a1368e287b28e9be8282de
abekshanov <b_and_work@mail.ru>
2020-04-02 20:35:32 +0300

    начал общую структуру: маршруты, контроллеры, миграции, представления, сервисы.

---
+++ database/migrations/2020_04_02_124958_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('parent_id')->nullable()->default(0);
+            $table->bigInteger('author_id');
+            $table->bigInteger('assignee_id')->nullable()->default(null);
+            $table->string('title');
+            $table->string('description');
+            $table->string('file_link')->nullable()->default(null);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2020_04_02_125946_update_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->bigInteger('author_id')->unsigned()->change();
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->bigInteger('assignee_id')->unsigned()->change();
+            $table->foreign('assignee_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('author_id');
+            $table->dropForeign('assignee_id');
+        });
+    }
+}
